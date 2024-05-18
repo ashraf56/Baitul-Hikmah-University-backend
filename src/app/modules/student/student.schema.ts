176b95f63ab826ccc159efec5cfef71f9c3ef269
ashraf56 <ashrafulfahim07@gmail.com>
2024-05-18 12:08:27 +0600

    enum used perfectly

--- src/app/modules/student/student.schema.ts
+++ src/app/modules/student/student.schema.ts
@@ -5,22 +5,26 @@ import { StudentsInfo } from './student.interface';
 
 const Userschema = new Schema<StudentsInfo>({
 
-name:{type:String , required:true},
-adress:{type:String},
-contactnumber:{type:String},
-country:{type:String},
-gender:['male','female'],
-gardian:{
-    fathersName: {type:String},
-    fathersNumber:{type:String}
-}
+    name: { type: String, required: true },
+    adress: { type: String },
+    contactnumber: { type: String },
+    country: { type: String },
+    gender: {
+        type: String,
+        enum: ["male", "female"],
+        required: true
+    },
+    gardian: {
+        fathersName: { type: String },
+        fathersNumber: { type: String }
+    }
 
 
 
 })
 
 
-const StudentsModal = model<StudentsInfo>("Student" , Userschema)
+const StudentsModal = model<StudentsInfo>("Student", Userschema)
 
 
 export default StudentsModal
