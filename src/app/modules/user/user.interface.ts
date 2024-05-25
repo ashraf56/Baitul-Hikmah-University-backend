cb64d467ea270e4eeb91cee15b57112b896a7402
ashraf56 <ashrafulfahim07@gmail.com>
2024-05-25 12:06:45 +0600

    user interface  created

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,11 @@
+interface UserInterface  {
+id:string;
+password:string;
+needsPasswordChange:boolean;
+role: 'admin' | 'student' | 'faculty';
+status: 'in-progress' | 'blocked';
+isDeleted: boolean;
+
+
+
+}
