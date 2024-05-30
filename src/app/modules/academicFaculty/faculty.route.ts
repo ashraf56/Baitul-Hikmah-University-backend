b83953c2ddbac629a6d1a0a26f569e622ebf9c2b
ashraf56 <ashrafulfahim07@gmail.com>
2024-05-30 12:34:09 +0600

    validate req added

--- src/app/modules/academicFaculty/faculty.route.ts
+++ src/app/modules/academicFaculty/faculty.route.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import { AcademicFacultyControllers } from "./faculty.controller";
+import validateRequest from "../../middleware/validateRequest";
+import { AcdemicFacultyValidation } from "./faculty.validation";
 
 
 const router = Router()
 
 
-router.post('/create-faculty', AcademicFacultyControllers.createAcdemicFacultyController)
-router.get('/',AcademicFacultyControllers.getAllAcademicFacultyController)
+router.post('/create-faculty', 
+validateRequest(AcdemicFacultyValidation.createAcdemicFacultyValidationSchema),
+    AcademicFacultyControllers.createAcdemicFacultyController)
+
+router.get('/', AcademicFacultyControllers.getAllAcademicFacultyController)
 router.get('/:id', AcademicFacultyControllers.getSingleAcademicFacultyController)
 router.patch('/:id', AcademicFacultyControllers.updateAcademicFacultyController)
 
