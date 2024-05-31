34f3fa2daa16b360f17be44b77a4cbbc0b78d948
ashraf56 <ashrafulfahim07@gmail.com>
2024-05-31 11:18:53 +0600

    academic create  department services created

--- src/app/modules/academicDepartment/department.service.ts
+++ src/app/modules/academicDepartment/department.service.ts
@@ -1,1 +1,15 @@
+import { AcademicDepartmentInterface } from "./department.interface";
+import academicDepartmentModel from "./department.model";
 
+const createAcademicDepartmentintoDB = async (payload: AcademicDepartmentInterface) => {
+
+    const insertDep = await academicDepartmentModel.create(payload)
+    return insertDep
+
+}
+
+
+
+export const academicDepartmentService = {
+    createAcademicDepartmentintoDB
+}
