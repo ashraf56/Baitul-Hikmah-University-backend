7c67e70fe0b8375a45be1e78d51a146995862b80
ashraf56 <ashrafulfahim07@gmail.com>
2024-06-02 16:59:31 +0600

    set  beta prmary validation error

--- dist/app/middleware/globalError.js
+++ dist/app/middleware/globalError.js
@@ -1,12 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const zod_1 = require("zod");
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 const GlobalErrorhandller = ((error, req, res, next) => {
-    const statuscode = error.statuscode || 500;
-    const message = error.message || "something error";
+    let statuscode = error.statuscode || 500;
+    let message = error.message || "something error";
+    const errorsource = [
+        {
+            path: '',
+            message: "something error "
+        }
+    ];
+    if (error instanceof zod_1.ZodError) {
+        {
+            statuscode = 401;
+            message = "amader validation error";
+        }
+    }
     return res.status(statuscode).json({
         success: false,
         message,
--- src/app/middleware/globalError.ts
+++ src/app/middleware/globalError.ts
@@ -1,15 +1,37 @@
 import { ErrorRequestHandler } from "express";
+import { ZodError } from "zod";
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-const GlobalErrorhandller:ErrorRequestHandler = ((error, req, res, next) => {
+const GlobalErrorhandller: ErrorRequestHandler = ((error, req, res, next) => {
 
-    const statuscode = error.statuscode || 500
- const  message = error.message || "something error"
+    let statuscode = error.statuscode || 500
+    let message = error.message || "something error"
+
+    type ErrorSource = {
+        path: '',
+        message: string
+    }[];
+
+    const errorsource: ErrorSource = [
+        {
+            path: '',
+            message: "something error "
+        }
+    ]
+
+    if (error instanceof ZodError) {
+        {
+            statuscode = 401
+            message = "amader validation error"
+
+        }
+    }
+
     return res.status(statuscode).json({
         success: false,
-       message,
+        message,
         error: error
     })
 
