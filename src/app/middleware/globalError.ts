e4bf5fdc272ad4c688a43f0bf284501cec242bd2
ashraf56 <ashrafulfahim07@gmail.com>
2024-06-02 11:25:15 +0600

    GLobal error handller edied

--- src/app/middleware/globalError.ts
+++ src/app/middleware/globalError.ts
@@ -1,9 +1,9 @@
-import { NextFunction, Request, Response } from "express";
+import { ErrorRequestHandler } from "express";
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-const GlobalErrorhandller = ((error: any, req: Request, res: Response, next: NextFunction) => {
+const GlobalErrorhandller:ErrorRequestHandler = ((error, req, res, next) => {
 
     return res.status(500).json({
         success: false,
