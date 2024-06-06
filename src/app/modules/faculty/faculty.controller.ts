fb9aee67c8dcfd352c4216a9f1703ebdc1206709
ashraf56 <ashrafulfahim07@gmail.com>
2024-06-06 10:55:50 +0600

    faculty  router  created

--- src/app/modules/faculty/faculty.controller.ts
+++ src/app/modules/faculty/faculty.controller.ts
@@ -3,53 +3,53 @@ import { FacultyServices } from "./faculty.service";
 
 const getSingleFaculty = catchasync(async (req, res) => {
     const { id } = req.params;
-    const result = await FacultyServices.getSingleFacultyFromDB( id );
-  
-res.status(200).json({
-    success: true,
-    message: 'Faculty is retrieved succesfully',
-    data: result,
-})
+    const result = await FacultyServices.getSingleFacultyFromDB(id);
 
-  });
-  
-  const getAllFaculties = catchasync(async (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: 'Faculty is retrieved succesfully',
+        data: result,
+    })
+
+});
+
+const getAllFaculties = catchasync(async (req, res) => {
     const result = await FacultyServices.getAllFacultiesFromDB(req.query);
-  
+
     res.status(200).json({
         success: true,
         message: 'Faculty are retrieved succesfully',
         data: result,
     })
-  });
-  
-  const updateFaculty = catchasync(async (req, res) => {
+});
+
+const updateFaculty = catchasync(async (req, res) => {
     const { id } = req.params;
     const { faculty } = req.body;
-    const result = await FacultyServices.updateFacultyIntoDB( id , faculty);
+    const result = await FacultyServices.updateFacultyIntoDB(id, faculty);
     res.status(200).json({
         success: true,
         message: 'Faculty is updated succesfully',
         data: result,
     })
-  
-  });
-  
-  const deleteFaculty = catchasync(async (req, res) => {
-    const {  id } = req.params;
+
+});
+
+const deleteFaculty = catchasync(async (req, res) => {
+    const { id } = req.params;
     const result = await FacultyServices.deleteFacultyFromDB(id);
-    
+
     res.status(200).json({
         success: true,
         message: 'Faculty is deleted succesfully',
         data: result,
     })
-    
-  });
-  
-  export const FacultyControllers = {
+
+});
+
+export const FacultyControllers = {
     getAllFaculties,
     getSingleFaculty,
     deleteFaculty,
     updateFaculty,
-  };
+};
