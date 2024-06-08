e6d7849a7069c63b2846eb957ad706a1b3925424
ashraf56 <ashrafulfahim07@gmail.com>
2024-06-08 15:38:51 +0600

    course ruter created

---
+++ dist/app/modules/course/course.route.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CourseRouter = void 0;
+const express_1 = require("express");
+const validateRequest_1 = __importDefault(require("../../middleware/validateRequest"));
+const course_validation_1 = require("./course.validation");
+const course_controller_1 = require("./course.controller");
+const router = (0, express_1.Router)();
+router.post('/create-course', (0, validateRequest_1.default)(course_validation_1.CourseValidations.createCourseValidationSchema), course_controller_1.CourseControllers.createCourseController);
+router.get('/:id', course_controller_1.CourseControllers.getSingleCourseController);
+router.delete('/:id', course_controller_1.CourseControllers.deleteCourseController);
+router.get('/', course_controller_1.CourseControllers.getAllCourseController);
+exports.CourseRouter = router;
---
+++ src/app/modules/course/course.route.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import validateRequest from "../../middleware/validateRequest";
+import { CourseValidations } from "./course.validation";
+import { CourseControllers } from "./course.controller";
+
+const router = Router();
+
+router.post(
+  '/create-course',
+  validateRequest(CourseValidations.createCourseValidationSchema),
+  CourseControllers.createCourseController,
+);
+
+router.get('/:id', CourseControllers.getSingleCourseController);
+
+
+
+router.delete('/:id', CourseControllers.deleteCourseController);
+
+
+
+
+router.get('/', CourseControllers.getAllCourseController);
+
+export const CourseRouter = router;
