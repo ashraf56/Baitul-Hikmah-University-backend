331f31b7e54fd96f966f9f9e1063ad4569f0e969
ashraf56 <ashrafulfahim07@gmail.com>
2024-06-10 14:53:36 +0600

    semisterRagistration validation created

---
+++ src/app/modules/semisterRegistration/semisterRagistration.validation.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+import { SemesterRegistrationStatus } from "./semisterRagistration.constants";
+
+const createSemesterRegistrationValidationSchema = z.object({
+    body: z.object({
+      academicSemester: z.string(),
+      status: z.enum([...(SemesterRegistrationStatus as [string, ...string[]])]),
+      startDate: z.string().datetime(),
+      endDate: z.string().datetime(),
+      minCredit: z.number(),
+      maxCredit: z.number(),
+    }),
+  });
+
+
+
+  export const semisterRagistrationValidation = {
+    createSemesterRegistrationValidationSchema
+  }
