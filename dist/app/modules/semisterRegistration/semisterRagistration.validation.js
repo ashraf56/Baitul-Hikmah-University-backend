4093b70db1d051f956001aec94ce5a11e2d79bdd
ashraf56 <ashrafulfahim07@gmail.com>
2024-06-10 15:44:01 +0600

    semisterRagistration  get route  created

--- dist/app/modules/semisterRegistration/semisterRagistration.validation.js
+++ dist/app/modules/semisterRegistration/semisterRagistration.validation.js
@@ -13,6 +13,17 @@ const createSemesterRegistrationValidationSchema = zod_1.z.object({
         maxCredit: zod_1.z.number(),
     }),
 });
+const upadateSemesterRegistrationValidationSchema = zod_1.z.object({
+    body: zod_1.z.object({
+        academicSemester: zod_1.z.string().optional(),
+        status: zod_1.z.enum([...semisterRagistration_constants_1.SemesterRegistrationStatus]).optional(),
+        startDate: zod_1.z.string().datetime().optional(),
+        endDate: zod_1.z.string().datetime().optional(),
+        minCredit: zod_1.z.number().optional(),
+        maxCredit: zod_1.z.number().optional(),
+    }),
+});
 exports.semisterRagistrationValidation = {
-    createSemesterRegistrationValidationSchema
+    createSemesterRegistrationValidationSchema,
+    upadateSemesterRegistrationValidationSchema
 };
