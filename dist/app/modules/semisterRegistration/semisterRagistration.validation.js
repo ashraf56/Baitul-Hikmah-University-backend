87da25d2bf35a63d3b762d2b3558232e9e21db9d
ashraf56 <ashrafulfahim07@gmail.com>
2024-06-10 14:54:08 +0600

    semisterRagistration validation created

--- dist/app/modules/semisterRegistration/semisterRagistration.validation.js
+++ dist/app/modules/semisterRegistration/semisterRagistration.validation.js
@@ -1,1 +1,18 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.semisterRagistrationValidation = void 0;
+const zod_1 = require("zod");
+const semisterRagistration_constants_1 = require("./semisterRagistration.constants");
+const createSemesterRegistrationValidationSchema = zod_1.z.object({
+    body: zod_1.z.object({
+        academicSemester: zod_1.z.string(),
+        status: zod_1.z.enum([...semisterRagistration_constants_1.SemesterRegistrationStatus]),
+        startDate: zod_1.z.string().datetime(),
+        endDate: zod_1.z.string().datetime(),
+        minCredit: zod_1.z.number(),
+        maxCredit: zod_1.z.number(),
+    }),
+});
+exports.semisterRagistrationValidation = {
+    createSemesterRegistrationValidationSchema
+};
