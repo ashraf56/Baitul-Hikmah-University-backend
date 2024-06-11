051584b38dcd466635a3c119b8709fe6a9f47f37
ashraf56 <ashrafulfahim07@gmail.com>
2024-06-11 10:19:24 +0600

    OfferedCourse router  created

--- dist/app/modules/OfferedCourse/OfferedCourse.route.js
+++ dist/app/modules/OfferedCourse/OfferedCourse.route.js
@@ -1,1 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.OfferedCourseRoute = void 0;
+const express_1 = require("express");
+const validateRequest_1 = __importDefault(require("../../middleware/validateRequest"));
+const OfferedCourse_validation_1 = require("./OfferedCourse.validation");
+const OfferedCourse_controller_1 = require("./OfferedCourse.controller");
+const router = (0, express_1.Router)();
+router.post('/create-offered-course', (0, validateRequest_1.default)(OfferedCourse_validation_1.OfferedCourseValidations.createOfferedCoursevalidation), OfferedCourse_controller_1.OfferedCourseController.createOfferedCourseController);
+exports.OfferedCourseRoute = router;
